107ad7c494eb9a918f53c3264a2cddc5bb765135
infinitesea7 <nicolas7711771177@gmail.com>
2024-08-15 14:13:03 +0200

    update

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  -- disable bufferline
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "right",
+      },
+    },
+  },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,11 @@
+return {
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "css",
+      },
+    },
+  },
+}
