474c9508ae7cd21ba7c7911649d4fca964c002e9
infinitesea7 <nicolas7711771177@gmail.com>
2024-08-16 15:50:40 +0200

    disable flash plugin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
+  { "folke/flash.nvim", enabled = false },
 }
